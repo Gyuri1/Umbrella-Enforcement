574bf163feef4d32b381d3941f9531049212b4f9
Gyuri1 <Acs.gyorgy1@gmail.com>
2021-08-31 18:41:54 +0200

    Add files via upload

--- umb-enf-domains.py
+++ umb-enf-domains.py
@@ -1,46 +1,31 @@
 #!/usr/bin/env python
 
 """
-TEST
-
-
+TEST:
 curl -i -X POST "https://management.api.umbrella.com/v1/organizations/<ORGID>/internaldomains" \
 -H 'Authorization: Basic <TOKEN>' \
 -H 'Content-Type: application/json' \
 -d '{ "domain": "Dtest" }'
-
-
-
 """
 import requests
 import base64
 
 OrgID="<ORGID>"
-
 url="https://management.api.umbrella.com/v1/organizations/"
-
 api_key=".."
 api_secret="..."
-
 # Substitute your key and secret.
 keySecret = api_key+":"+api_secret
 
 # Encode and print your credentials
 token = base64.b64encode(keySecret.encode())
 encoding = 'utf-8'
-print(token.decode(encoding))
-
 payload='{ "domain": "Dtest3", "description":"Dtest3","includeAllVAs":false,"includeAllMobileDevices":false }'
-
 headers = {
   'Authorization': 'Basic '+str(token, encoding),
   'Content-Type': 'application/json'
 }
 
-url_users= url + OrgID+"/internaldomains"
-
-print(url_users)
-print(headers )
-response = requests.request("POST", url_users, headers=headers, data=payload)
-
+url_domains= url + OrgID+"/internaldomains"
+response = requests.request("POST", url_domains, headers=headers, data=payload)
 print(response.text)
